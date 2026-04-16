ccb246e3699ca1e976b26e25f9c137bd9e6a5aec
bwearly <admin@stylessync.com>
2026-04-16 16:12:17 -0600

    updated the database string

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,10 +1,10 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not set");
+if (!process.env.TRAWL_DATABASE_DATABASE_URL) {
+  throw new Error("TRAWL_DATABASE_DATABASE_URL is not set");
 }
 
-const sql = neon(process.env.DATABASE_URL);
+const sql = neon(process.env.TRAWL_DATABASE_DATABASE_URL);
 
 export const db = drizzle(sql);
